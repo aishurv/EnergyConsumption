cb36870e414b7a477d03fa14b7805aeb13f14cc9
Laxmi Wandare <95848361+aishurv@users.noreply.github.com>
2024-05-13 23:45:28 -0700

    Update app.py

--- app.py
+++ app.py
@@ -35,18 +35,18 @@ def main():
     # Create input fields for user input
     temperature = st.number_input('Temperature (In Degree Celsius)', min_value=-0.0, max_value=50.0, value=20.0)
     humidity = st.number_input('Humidity (In Percentage In Air )', min_value=0.0, max_value=100.0, value=50.0)
-    wind_speed = st.number_input('Wind Speed', min_value=0.0, max_value=50.0, value=10.0)
-    general_diffuse_flows = st.number_input('General Diffuse Flows', min_value=0.0, max_value=1.0, value=0.5)
+    wind_speed = st.number_input('Wind Speed (in meter/second)', min_value=0.0, max_value=50.0, value=10.0)
+    general_diffuse_flows = st.number_input('General Diffuse Flows ()', min_value=0.0, max_value=1.0, value=0.5)
     diffuse_flows = st.number_input('Diffuse Flows', min_value=0.0, max_value=1.0, value=0.5)
-    month = st.number_input('Month', min_value=1, max_value=12, value=1)
-    day = st.number_input('Day', min_value=1, max_value=31, value=1)
-    hour = st.number_input('Hour', min_value=0, max_value=23, value=0)
-    minute = st.number_input('Minute', min_value=0, max_value=59, value=0)
+    month = st.number_input('Month(In number 1 to 12)', min_value=1, max_value=12, value=1)
+    day = st.number_input('Day (In number 1 to 31)', min_value=1, max_value=31, value=1)
+    hour = st.number_input('Hour (In number 0 to 23)', min_value=0, max_value=23, value=0)
+    minute = st.number_input('Minute (In Number 0 to 59)', min_value=0, max_value=59, value=0)
 
     if st.button('Predict'):
         # Make prediction when the button is clicked
         predicted_power = predict_power_consumption(temperature, humidity, wind_speed, general_diffuse_flows, diffuse_flows, month, day, hour, minute)
-        st.write(f'Predicted Average Power Consumption: {predicted_power} W/h')
+        st.write(f'Predicted Average Power Consumption: {predicted_power} W/h for the next 10 minutes from provided time <br> Provided Time {hour}:{minute} and date {day}/{month}    ')
 
 if __name__ == "__main__":
     main()
